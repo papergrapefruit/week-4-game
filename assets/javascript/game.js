f0edaf032771e3c4be7933b8f78f09fb70e4d3ee
papergrapefruit <statofeld@gmail.com>
2017-06-25 19:14:11 -0400

    adding

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,23 +4,31 @@ var pointVal = 0;
 //var randomStart (randomly generated between 19 & 120) is the number the player should match to win the game
 var randomStart = Math.floor((Math.random() * 102) + 19);
   var random = Math.floor((Math.random() * 13) + 1);
+    //each crystal "Val" (randomly generated between 1 & 12) is the number of points awarded to each crystal on click
     var smokyVal = Math.floor((Math.random() * 13) + 1);
     var amethystVal=Math.floor((Math.random() * 13) + 1);
     var snowVal=Math.floor((Math.random() * 13) + 1);
     var saltVal=Math.floor((Math.random() * 13) + 1);
-
-//each crystal "Val" (randomly generated between 1 & 12) is the number of points awarded to each crystal on click
 $(document).ready(function(){
   $("#toMatch").text(randomStart);
   $("#points").text(pointVal);
+  $("#wins").text(wins);
+  $("#losses").text(losses);
+  //smoky crystal on button click options
   $("#smoky").click(function(){
     $("#points").text(pointVal+=smokyVal);
     if (pointVal < randomStart) {
       $("#numberSet").text("Keep going!");
     } else if (pointVal === randomStart) {
       $("#numberSet").text("You did it!!");
-    } else {
-  $("#numberSet").text("Alas, your crystals deceived you! Would you like to play again?");
+      wins++;
+      $("#wins").text(wins);
+      $("#smoky").attr('disabled','disabled');
+}   else {
+  $("#numberSet").text("Alas, the crystals deceived you! Would you like to play again?");
+  losses++;
+    $("#losses").text(losses);
+    $("#smoky").attr('disabled','disabled');
 }
   });
 $("#amethyst").click(function(){
@@ -31,9 +39,13 @@ $("#amethyst").click(function(){
       if (pointVal < randomStart) {
         $("#numberSet").text("Guess again!");
       } else if (pointVal === randomStart) {
+        wins++;
+      $("#wins").text(wins);
           $("#numberSet").text("You rock!!! What a winner!");
       } else {
       $("#numberSet").text("Oh, no! This wasn't your best round. Would you like to play again?");
+        losses++;
+        $("#losses").text(losses);
       }
 });
 $("#snow").click(function(){
@@ -42,13 +54,15 @@ $("#snow").click(function(){
   };
     $("#points").text(pointVal+=snowVal);
     if (pointVal < randomStart) {
-      $("#numberSet").text("Don't stop!");
+      $("#numberSet").text("Don't stop-you're on a roll!");
     } else if (pointVal === randomStart) {
+      wins++;
+    $("#wins").text(wins);
         $("#numberSet").text("Victory!!");
-        $("wins").append(" <b>1</b>");
     } else {
-    $("#numberSet").text("Well, that wasn't so good... play again?");
-    $("losses").append(" <b>1+=</b>");
+    $("#numberSet").text("Well, that was a click too far... play again?");
+    losses++;
+      $("#losses").text(losses);
     }
   });
 $("#salt").click(function(){
@@ -59,9 +73,13 @@ $("#salt").click(function(){
     if (pointVal < randomStart) {
       $("#numberSet").text("Please continue!");
     } else if (pointVal === randomStart) {
+    wins++;
+    $("#wins").text(wins);
         $("#numberSet").text("It's crystal clear - you're a winner!");
     } else {
-    $("#numberSet").text("Well, well, well... didn't happen for you this time. Play again?");
+    $("#numberSet").text("Oh, well.. didn't happen for you this time. Play again?");
+    losses++;
+      $("#losses").text(losses);
     }
   })
 });
